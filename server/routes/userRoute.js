5d84983622011708cd7d58df307c99d121e010bf
harsh7598 <harryht7@gmail.com>
2025-04-27 15:12:14 +0530

    userregistration

---
+++ server/routes/userRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { register } from '../controllers/userController.js';
+
+const userRouter = express.Router();
+
+userRouter.post('/register', register);
+
+userRouter.post('/register', register)
+
+
+export default userRouter
