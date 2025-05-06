3492209fa27c7c83eab8a450cd17af8ce0053674
harsh7598 <harryht7@gmail.com>
2025-05-06 16:06:49 +0530

    userLogin

---
+++ server/middleware/authUser.js
@@ -0,0 +1,10 @@
+const authUser = async (req, res, next) => {
+    const {token} = req.cookies;
+
+    if(!token){
+        return res.json({success: false, message: 'Not Authorized'});
+    }
+    try {
+        const tokenDecode = 
+    }
+}
